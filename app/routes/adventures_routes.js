123d0612db546318bb711f64a1cc2fd12ddfe895
patrickga <patrick.g.alvarez@gmail.com>
2022-04-02 13:36:36 -0500

    Updated host

---
+++ app/routes/adventures_routes.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const passport = require('passport')
+
+const customErrors = require('../../lib/custom_errors')
+
+//this function sends a 404 when non-existent document is requested
+const handle404 = customErrors.handle404
+
+//middleware that can send a  401 when a user tries to access something they do not own
+const requireOwnership = customErrors.requireOwnership
+//requireToken is passed as a second arg to router.<verb>
+//makes it so that a token MUST be passed for that route to be available --> also sets 'req.user'
+const requireToken = passport.authenticate('bearer', {session: false})
+
+const removeBlanks = require('../../lib/remove_blank_fields')
+const { handle } = require('express/lib/application')
+
+const router = express.Router()
+
+//ROUTES GO HERE
+
+//INDEX ROUTE
+//Get adventures 
+router.get('/adventures', (req, res)=>{
+    Adventure.find()
+    .populate('owner')
+        .then(adventures =>{
+            return adventures.map(adventure => adventure.toObject())
+        })
+        .then(adventures =>{
+            res.status(200).json({adventures: adventures})
+        })
+        .catch(next)
+})
+
+
+//ROUTES ABOVE HERE
+
+
+
+
+module.exports = router
